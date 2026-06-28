796003b84181adff600c4ae01fd6bd0d62923bf9
ucihafafa-alt <ucihafafa@gmail.com>
2026-06-28 17:36:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const KEY='AQ.Ab8RN6Jsrf24BA3A08e3kuATarokC3KFCThMadNO18KmUo-v0w';
+const KEY='AQ.Ab8RN6Jd6AF202jNdIU7npMz19RpxAJ_3Hlzk27bZn33NJCwZw
 const API='https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key='+KEY;
 const state={left:null,right:null,leftType:null,rightType:null,leftData:null,rightData:null};
 const $=id=>document.getElementById(id);
