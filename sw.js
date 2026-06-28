2d94ffb1d1fad13de9c7c63c953ba5dfcfa2f4e8
ucihafafa-alt <ucihafafa@gmail.com>
2026-06-28 21:28:22 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE='sara-palm-pdf-final-20260628';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','style.css','app.js','manifest.json','sara.jpg','icon.png','icon-192.png']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
