7eeea528bbe2efe8ed642d44446af34321c02358
ucihafafa-alt <ucihafafa@gmail.com>
2026-06-29 15:05:37 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='sara-palm-v43-upload-fix-1';
+const CACHE='sara-palm-v20-print-pdf-word';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','style.css','app.js','manifest.json','sara.jpg','icon.png','icon-192.png']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
